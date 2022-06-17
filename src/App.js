07466efb70b118b528a09417671cd08e69310042
JungUk <62638035+JungUk@users.noreply.github.com>
2022-06-17 15:14:33 +0900

    6.17

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
 
-  console.log("I run all the time");
-
-  const iRunOnlyOnce = () => {
+  useEffect(() => {
     console.log("I run only once");
-  }
+  }, []);
+  
+  useEffect(() => {
+    console.log("I run when 'keyword' changes")
+  }, [keyword]);
 
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("I run when 'counter' changes")
+  }, [counter]);
+  
+  useEffect(() => {
+    console.log("I run when 'keyword and counter' change")
+  }, [keyword, counter]);
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
