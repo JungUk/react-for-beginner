2a3cb6003573cbb92e87686380688b53c47575e5
JungUk <62638035+JungUk@users.noreply.github.com>
2022-06-20 00:31:36 +0900

    6.20

--- src/App.js
+++ src/App.js
@@ -13,13 +13,19 @@ function App() {
     setTodos(currentArray => [toDo, ...currentArray] );
     setTodo("");
   }
+  console.log(toDos);
   return ( 
 
     <div>
+      <h1>My to dos {toDos.length}</h1>
       <form onSubmit={onSubmit}>
       <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
       <button>Add to do</button>
       </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
