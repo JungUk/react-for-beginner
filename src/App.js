b372ec9cda28c3940f5f2e867747ce19cda1e32a
JungUk <62638035+JungUk@users.noreply.github.com>
2022-06-19 23:58:36 +0900

    6.19

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
   const onChange = (event) => setTodo(event.target.value);
   const onSubmit = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     if(toDo === "") {
       return;
     }
+    setTodos(currentArray => [toDo, ...currentArray] );
     setTodo("");
   }
   return ( 
+
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
-        <button>Add to do</button>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."></input>
+      <button>Add to do</button>
       </form>
     </div>
   );
